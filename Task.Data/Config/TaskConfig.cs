90b56198da297d058acb84e769afa89e5e8f242e
Lahiru Madushan <lahiru.mirihagoda@gmail.com>
2023-05-17 23:49:34 +0530

    Docker compose changes

---
+++ Task.Data/Config/TaskConfig.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManage.ViewModels;
+
+namespace TaskManage.Data.Config
+{
+    public class TaskConfig : IEntityTypeConfiguration<TaskVM>
+    {
+
+        public void Configure(EntityTypeBuilder<TaskVM> builder)
+        {
+            builder.ToTable("Task");
+        }
+    }
+}
